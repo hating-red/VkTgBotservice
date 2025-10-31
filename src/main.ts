bf7a08d3e3a40c8128b6e1a4a423c68d0b56b617
SaveliyShutov <savelijsutov@gmail.com>
2025-10-31 16:16:28 +0500

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [process.env.MAIN_BACKEND_URL],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 2006);
+  console.log(`🚀 Сервер слушает ${process.env.PORT ?? 2006}`);
 }
+
 bootstrap();
